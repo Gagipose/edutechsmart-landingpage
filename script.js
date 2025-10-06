cd42a4a62b9bf11fcbea953f94d03943d8463ab7
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2025-10-06 09:58:18 +0200

    klar (#2), (#5)

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menuIcon = document.querySelector("#menu-icon");
 const menu = document.querySelector("#menu");
+const menuContainer = document.querySelector("#menu-container")
 
 menuIcon.addEventListener("click", () => {
     menu.classList.toggle("open")
+    menuContainer.classList.toggle("open")
+    
+    if (menu.classList.contains("open")) {
+        menuIcon.innerText = "X"
+    } else {
+        menuIcon.innerText = "☰"
+    }
 });
